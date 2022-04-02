7c23a7d8fff8d6dcd9458bff95651760970f71de
Philipp Lackner <mail@pl-coding.com>
2022-04-02 14:01:54 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.plcoding.stockmarketapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.stockmarketapp.ui.theme.StockMarketAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockMarketAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
