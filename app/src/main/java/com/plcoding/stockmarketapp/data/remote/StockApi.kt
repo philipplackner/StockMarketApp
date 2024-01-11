3fec234b61ac89de1b95334c5389784949d61422
Rodrigo Bellanti <rodrigobellanti@gmail.com>
2024-01-11 17:24:49 -0300

    Checkpoint 1 api & db setup (#1)
    * structure
    
    * api and db

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -0,0 +1,18 @@
+package com.plcoding.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+    companion object {
+        const val API_KEY = "C0OQTQ4Q6UCSTM3H"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+}
